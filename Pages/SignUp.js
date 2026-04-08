4320da160730876d64747915420c482f790502f3
Umer Shafiq <mumer04250@gmail.com>
2026-04-09 00:11:05 +0500

    Initial Playwright framework

---
+++ Pages/SignUp.js
@@ -0,0 +1,13 @@
+import {expect} from '@playwright/test'
+export class userSignup{
+    /** @param {import('@playwright/test').Page} page */
+    constructor(page)
+    {
+        this.page=page
+        
+        
+    }
+    
+    
+    
+}
