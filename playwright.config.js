fcd5cb9a3f50378a50794efc289b0f720110fea8
Umer <umer2030@gmail.com>
2026-04-11 19:07:25 +0500

    added CI/CD workflow

--- playwright.config.js
+++ playwright.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
+  
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -23,7 +24,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+ 
+  reporter: [['html', { open: 'never' }]],
   
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
